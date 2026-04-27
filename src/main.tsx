12ba3c01f2881c97f9f7b2d7d93470cbdd2c2d52
koroush bagheri <kooroshbagheri97director@gmail.com>
2026-04-27 16:05:49 +0330

    feat: setup app entry with RouterProvider and DarkModeProvider
    - Initialize React root with createRoot
    - Wrap app with StrictMode
    - Add global styles
    - Configure React Router using RouterProvider
    - Add DarkMode context provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./globalStyle/globalStyle.css";
 import { DarkModeProvider } from "./context/theme/DarkModeProvider.tsx";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/Router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <DarkModeProvider>
-      <App />
+      <RouterProvider router={router} />
     </DarkModeProvider>
   </StrictMode>,
 );
