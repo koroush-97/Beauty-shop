a156b4ff5b7d2eabb8dd3d8f44715c9a3a9af70a
koroush bagheri <kooroshbagheri97director@gmail.com>
2026-04-27 16:05:19 +0330

    feat: create main App layout
    - Add Navbar and Footer components
    - Add Outlet for rendering nested routes
    - Setup basic page container structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
-export default function App() {
-  return <div>hello word</div>;
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+function App() {
+  return (
+    <div className="container ">
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
+
+export default App;
