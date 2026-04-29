5cd1f2d5afeeb402ccf4abe7c4549be093c30664
koroush bagheri <kooroshbagheri97director@gmail.com>
2026-04-30 00:46:50 +0330

    feat: add Navlinks for navbar megamenu and impelement icons and links

---
+++ src/components/navlinks/NavLinks.tsx
@@ -0,0 +1,75 @@
+import { IoMdArrowDropdown } from "react-icons/io";
+import { NavLink } from "react-router-dom";
+
+type NavLinksProps = {
+  setshow: () => void;
+};
+
+export default function NavLinks({ setshow }: NavLinksProps) {
+  return (
+    <ul className="flex flex-row-reverse p-2 lg:gap-x-5  text-[10px] lg:text-[14px] flex-wrap gap-y-2">
+      <li>
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            isActive ? "nav-link nav-link-active" : "nav-link"
+          }
+        >
+          <span className="  w-10 text-[14px]"> صفحه اصلی</span>
+        </NavLink>
+      </li>
+
+      <li className="hover:font-bold  flex flex-row" onClick={setshow}>
+        <div className="nav-link cursor-pointer hover:text-text flex flex-row justify-center items-center">
+          <span className="pt-1.5">
+            <IoMdArrowDropdown />
+          </span>
+          <span className=" text-[14px]">دسته بندی محصول</span>
+        </div>
+      </li>
+
+      <li>
+        <NavLink
+          to={"/Trackorders"}
+          className={({ isActive }) =>
+            isActive ? "nav-link nav-link-active" : "nav-link"
+          }
+        >
+          <span className=" text-[14px]">پیگیری سفارشات</span>
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink
+          to={"/weblog"}
+          className={({ isActive }) =>
+            isActive ? "nav-link nav-link-active" : "nav-link"
+          }
+        >
+          <span className=" text-[14px]">وبلاگ</span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={"/callus"}
+          className={({ isActive }) =>
+            isActive ? "nav-link nav-link-active" : "nav-link"
+          }
+        >
+          <span className=" text-[14px]">تماس با ما</span>
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink
+          to={"/about"}
+          className={({ isActive }) =>
+            isActive ? "nav-link nav-link-active" : "nav-link"
+          }
+        >
+          <span className=" text-[14px]">درباره ما</span>
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
