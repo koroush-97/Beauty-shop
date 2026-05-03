f9d8c099667fb266900128d193a45e0ffb4422ed
koroush bagheri <kooroshbagheri97director@gmail.com>
2026-05-03 21:06:08 +0330

    feat: add titlecomponent and impelement it

---
+++ src/components/titleItem/TitleItem.tsx
@@ -0,0 +1,25 @@
+type titleItemProps = {
+  //   children: React.ReactNode;
+  header?: string;
+  content?: string;
+  icon?: React.ReactNode;
+};
+
+export default function TitleItem({
+  //   children,
+  header,
+  content,
+  icon,
+}: titleItemProps) {
+  return (
+    <div className="flex flex-row gap-x-1">
+      <div className=" flex flex-col justify-end text-right">
+        <h1 className="font-bold text-bg">{header}</h1>
+        <p className=" text-sm">{content}</p>
+      </div>
+      <div className=" flex justify-center items-center">
+        <span>{icon}</span>
+      </div>
+    </div>
+  );
+}
