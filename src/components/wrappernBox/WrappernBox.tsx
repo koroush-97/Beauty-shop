88306eb70aa6b39a977079973131482304c03193
koroush bagheri <kooroshbagheri97director@gmail.com>
2026-05-05 21:11:50 +0330

    feat(ui): create WrappernBox component
    - reusable wrapper container
    - supports dynamic width and height via props
    - includes hover shadow transition styling

---
+++ src/components/wrappernBox/WrappernBox.tsx
@@ -0,0 +1,20 @@
+type WrappernProps = {
+  children?: React.ReactNode;
+  width?: number | string;
+  height?: number | string;
+};
+
+export default function WrappernBox({
+  children,
+  width = "full",
+  height = "full",
+}: WrappernProps) {
+  return (
+    <div
+      className="transition-shadow  hover:shadow-[0_8px_24px_-6px_rgba(0,0,0,0.8)] duration-500 rounded-2xl p-2 shadow-[0_8px_24px_-6px_rgba(0,0,0,0.3)]"
+      style={{ width, height }}
+    >
+      {children}
+    </div>
+  );
+}
