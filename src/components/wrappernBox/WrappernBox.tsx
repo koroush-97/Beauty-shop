27a0e43650d34e577c48b47a5270953c065d8d80
koroush bagheri <kooroshbagheri97director@gmail.com>
2026-05-06 21:12:43 +0330

    refactor(wrapper-box): support custom classname prop

--- src/components/wrappernBox/WrappernBox.tsx
+++ src/components/wrappernBox/WrappernBox.tsx
@@ -2,16 +2,18 @@ type WrappernProps = {
   children?: React.ReactNode;
   width?: number | string;
   height?: number | string;
+  classname?: string;
 };
 
 export default function WrappernBox({
   children,
   width = "full",
   height = "full",
+  classname,
 }: WrappernProps) {
   return (
     <div
-      className="transition-shadow  hover:shadow-[0_8px_24px_-6px_rgba(0,0,0,0.8)] duration-500 rounded-2xl p-2 shadow-[0_8px_24px_-6px_rgba(0,0,0,0.3)]"
+      className={` ${classname} transition-shadow  hover:shadow-[0_8px_24px_-6px_rgba(0,0,0,0.8)] duration-500 rounded-2xl p-2 shadow-[0_8px_24px_-6px_rgba(0,0,0,0.3)]`}
       style={{ width, height }}
     >
       {children}
