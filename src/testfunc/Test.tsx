9485673ab5c57f88ab654816d5edb599e10106a9
koroush bagheri <kooroshbagheri97director@gmail.com>
2026-05-07 23:29:07 +0330

    feat: add (test) component to try logics and test it

---
+++ src/testfunc/Test.tsx
@@ -0,0 +1,44 @@
+// interface FeatureBoxProps {
+//   variant: "IconBtn" | "FeuterBtn" | "ButtonChance";
+//   title?: string;
+//   description?: string;
+//   image?: string;
+//   width?: string;
+//   height?: string;
+// }
+
+// {
+//   IconBtn && (
+//     <div className="flex p-1 gap-x-1 py-5" style={{ width, height }}>
+//       {/* Text Section */}
+//       <div
+//         dir="rtl"
+//         className="p-2 flex flex-col flex-[2/3] w-[70%] justify-center items-center"
+//       >
+//         <h3 className="lg:text-[14px] font-semibold ">{title}</h3>
+//         <p className="text-[10px] md:text-[11px] lg:text-[12px] ">
+//           {description}
+//         </p>
+//       </div>
+
+//       {/* Icon Section */}
+//       <div
+//         dir="rtl"
+//         className="relative flex justify-center items-center w-[30%] p-2 border-l-2 border-lightback"
+//       >
+//         <div className="absolute -right-6.5 text-lightback">
+//           <IoMdArrowDropleft size={50} />
+//         </div>
+//         <img src={image} alt={title} className="object-fit w-[80%] h-[80%]" />
+//       </div>
+//     </div>
+//   );
+// }
+// {
+//   FeuterBtn && (
+//     <div>
+//       {" "}
+//       <button>this is test</button>{" "}
+//     </div>
+//   );
+// }
