d732f8819317ed12f98465d0f12ca1d79c552361
koroush bagheri <kooroshbagheri97director@gmail.com>
2026-05-08 11:39:31 +0330

    feat: (MainBtn) create new component

---
+++ src/components/buttons/mainBtn/MainBtn.tsx
@@ -0,0 +1,13 @@
+type mainBtnprops = {
+  text: string;
+};
+
+export default function MainBtn({ text }: mainBtnprops) {
+  return (
+    <button className=" rounded-bl-[20px] rounded-tr-[20px] p-1 cursor-pointer bg-[#e4e7f0] hover:bg-[#111171] group duration-300">
+      <p className="p-2 text-[#5a5c83] text-[10px] md:text-[14px]  group-hover:text-white font-semibold">
+        {text}
+      </p>
+    </button>
+  );
+}
