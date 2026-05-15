329b1e7bfcf499c2d32b975ad988fcfaf3a34a53
koroush <kooroshbagheri97director@gmail.com>
2026-05-15 23:10:46 +0330

    feat: add CategoryFourPartCard and ChooseCards exports

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,3 +1,5 @@
 export { default as CategoryCard } from "./categoryCard";
 export { default as NewProductCard } from "./newProductCard";
 export { default as OfferCard } from "./offercard";
+export { default as CategoryFourPartCard } from "./categorydiffrentCards";
+export { default as ChooseCards } from "./chooseCards";
