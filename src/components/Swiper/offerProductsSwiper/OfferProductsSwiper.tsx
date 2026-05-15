8a7ea33fdb56310dced1fd95c7f9306a8b9e63da
koroush <kooroshbagheri97director@gmail.com>
2026-05-15 23:13:21 +0330

    refactor: update OfferProductsSwiper to use OfferCard variants and add overlay label

--- src/components/Swiper/offerProductsSwiper/OfferProductsSwiper.tsx
+++ src/components/Swiper/offerProductsSwiper/OfferProductsSwiper.tsx
@@ -2,14 +2,14 @@
 // import NewProductCard from "../../newProductCard";
 // import { MainBtn } from "../../buttons";
 import { OfferCard } from "../../cards";
-import { AddButton } from "../../buttons";
+
 // @ assetess
 
 // import Tsheirtmale from "../../../assets/photo/topWeekSell/Tshirt-2.jpg";
 // import shortmale from "../../../assets/photo/topWeekSell/jean-low.jpg";
 // import scarf from "../../../assets/photo/topWeekSell/scarf.jpg";
 // import capboy from "../../../assets/photo/topWeekSell/cupbpy.jfif";
-import shomiz from "../../../assets/photo/topWeekSell/shomiz-01.jpg";
+// import shomiz from "../../../assets/photo/topWeekSell/shomiz-01.jpg";
 
 // @ swiper
 // import { Autoplay } from "swiper/modules";
@@ -23,7 +23,19 @@ export default function OfferProductsSwiper({
   classnameContainerSwiper,
 }: offerProductsSwipertype) {
   return (
-    <div className={` h-full ${classnameContainerSwiper} `}>
+    <div
+      className={` h-full w-full min-w-0 mySwiper relative  overflow-hidden  ${classnameContainerSwiper} `}
+    >
+      <div className="relative z-50">
+        {/* overlay */}
+
+        {/* text */}
+        <div className="absolute top-0 right-0 z-60 p-2 rounded-tl-2xl rounded-bl-2xl backdrop-blur-3xl   m-1  ">
+          <span className="pointer-events-none absolute inset-0 z-60  rounded-tl-2xl"></span>
+          <h3 className="font-semibold text-[14px]">پیشنهاد لحظه ای</h3>
+        </div>
+      </div>
+
       <Swiper
         style={{} as React.CSSProperties}
         slidesPerView={1}
@@ -35,24 +47,14 @@ export default function OfferProductsSwiper({
         loop={true}
       >
         <SwiperSlide className=" h-full bg-bg rounded-2xl ">
-          <OfferCard
-            button={
-              <div className=" h-full w-[50%] md:w-[80%]  ">
-                {" "}
-                <AddButton variant="blue" />
-              </div>
-            }
-            price={270000}
-            headers="شومیز زنانه مدل اس ار تی 7 یک آگهی بیخود"
-            offer={20}
-            subjectbrand={35}
-            imgsrc={shomiz}
-          />
+          <OfferCard variant="modal1" />
         </SwiperSlide>
-        <SwiperSlide className="  h-full bg-bg rounded-2xl">slid2</SwiperSlide>
-        <SwiperSlide className=" h-full bg-bg rounded-2xl">slid3 </SwiperSlide>
-        <SwiperSlide className=" h-full bg-bg rounded-2xl"> slid4</SwiperSlide>
-        <SwiperSlide className=" h-full bg-bg rounded-2xl">slid5 </SwiperSlide>
+        <SwiperSlide className="   h-75 bg-bg rounded-2xl">
+          <OfferCard variant="modal2" />
+        </SwiperSlide>
+        <SwiperSlide className="  h-75 bg-bg rounded-2xl">2</SwiperSlide>
+        <SwiperSlide className="  h-75 bg-bg rounded-2xl"> 3</SwiperSlide>
+        <SwiperSlide className="   h-75 bg-bg rounded-2xl">4</SwiperSlide>
       </Swiper>
     </div>
   );
