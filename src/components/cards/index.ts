a8f181b4fb483b2f59ccec50ca9defaaa0071da3
koroush <kooroshbagheri97director@gmail.com>
2026-05-16 23:55:37 +0330

    feat : add new export Customercard

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -3,3 +3,4 @@ export { default as NewProductCard } from "./newProductCard";
 export { default as OfferCard } from "./offercard";
 export { default as CategoryFourPartCard } from "./categorydiffrentCards";
 export { default as ChooseCards } from "./chooseCards";
+export { default as CustomerCard } from "./customerCard";
