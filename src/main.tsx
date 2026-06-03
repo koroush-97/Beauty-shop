f43ce9a06b045e2b6518b3337d9ea8e99d685664
koroush <kooroshbagheri97director@gmail.com>
2026-06-03 12:00:00 +0330

    Integrate React Query, Devtools and React Hot Toast in app root

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,28 @@ import "./globalStyle/globalStyle.css";
 import { DarkModeProvider } from "./context/theme/DarkModeProvider.tsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/Router.tsx";
+import { Toaster } from "react-hot-toast";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: 1,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <DarkModeProvider>
-      <RouterProvider router={router} />
-    </DarkModeProvider>
+    <QueryClientProvider client={queryClient}>
+      <DarkModeProvider>
+        <Toaster position="top-center" />
+        <RouterProvider router={router} />
+      </DarkModeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
