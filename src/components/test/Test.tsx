ac7ec10da54358c124c65372c8d349c014f6d04a
koroush <kooroshbagheri97director@gmail.com>
2026-06-10 12:00:00 +0330

    Update Test component for component-level testing

--- src/components/test/Test.tsx
+++ src/components/test/Test.tsx
@@ -1,22 +1,3 @@
-// import { Children } from "react";
-// import { useForm } from "react-hook-form";
-
-// export default function Test() {
-//    const {  handleSubmit ,  } = useForm({
-//     defaultValues:
-//    })
-
-// const onSubmit = async (values : any ) => {
-//   console.log('form data' , values)
-// }
-
-//   return (
-//     <div>
-//       <form onSubmit={handleSubmit(onSubmit)} >{Children}</form>
-//     </div>
-//   );
-// }
-
 export default function Test() {
   return <div>Test</div>;
 }
