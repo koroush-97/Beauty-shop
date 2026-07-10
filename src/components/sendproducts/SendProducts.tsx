2cdf7b8cf877ace002552ed98f3dba3f4f23727c
koroush <kooroshbagheri97director@gmail.com>
2026-07-10 12:00:00 +0330

    feat(orders): add sent products list view
    Created SendProducts page using OrdersList with shipped and delivered status filters.

--- src/components/sendproducts/SendProducts.tsx
+++ src/components/sendproducts/SendProducts.tsx
@@ -1,3 +1,10 @@
+import OrdersList from "../OrderList/OrderList";
+
 export default function SendProducts() {
-  return <div>SendProducts</div>;
+  return (
+    <OrdersList
+      statuses={["shipped", "delivered"]}
+      emptyMessage="سفارش ارسال‌شده‌ای ندارید."
+    />
+  );
 }
